279db978e39e56003ce7b0d6600bdb26cfd95b47
Nanda-Souza <fesouoli@gmail.com>
2023-02-09 17:02:58 -0300

    fix: Method postCustomer CPF validation

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -3,7 +3,7 @@ import joi from 'joi'
 export const customersSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().length(11).required(),
+    cpf: joi.string().length(11).pattern(/^[0-9]+$/).required(),
     birthday: joi.date().required()
 });
 
