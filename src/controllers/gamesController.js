5aee91cb8457a7bb1a24a06543ddbcbd27447a06
Nanda-Souza <fesouoli@gmail.com>
2023-02-09 13:11:03 -0300

    feat: Method postGames

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -10,3 +10,36 @@ export const getGames = (async (req, res) => {
       }
     
 })
+
+export const createGames = (async (req, res) => {
+    const { name,
+            image,
+            stockTotal,
+            pricePerDay } = req.body
+    
+    const game = {name, image, stockTotal, pricePerDay}
+
+    try {
+
+        const gameExist = await db.query(`SELECT name FROM games WHERE name = $1`, [name]);
+        
+        if (gameExist.rowCount > 0){
+            return res.status(409).send("Game already registered!");
+            
+
+        }
+
+        await db.query(
+            `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay]);
+
+
+    res.sendStatus(201)
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+      
+    res.status(201);
+    
+    
+})
--- src/routes/gamesRoute.js
+++ src/routes/gamesRoute.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { gamesSchema } from "../schemas/gamesSchema.js"
-import { getGames } from "../controllers/gamesController.js"
+import { getGames,
+        createGames } from "../controllers/gamesController.js"
 
 
 const gamesRoute = Router()
 
 gamesRoute.get("/games", getGames)
 
-gamesRoute.post("/games", validateSchema(gamesSchema))
+gamesRoute.post("/games", validateSchema(gamesSchema), createGames)
 
 
 export default gamesRoute
