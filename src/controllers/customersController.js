590362ad2c6a591a485991f1d627d1e03413cbb5
Nanda-Souza <fesouoli@gmail.com>
2023-02-09 16:14:39 -0300

    feat: Method getCustomersById

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -11,3 +11,22 @@ export const getCustomers = (async (req, res) => {
       }
     
     })
+
+    export const getCustomersById = (async (req, res) => {
+
+      try {
+          const { id } = req.params;
+          const result = await db.query( `SELECT * FROM customers WHERE id = ${id};`);
+          
+          if (result.rowCount === 0){
+            return res.status(404).send("Customer not found!");
+            
+          }
+          res.status(200).send(result.rows[0]);
+
+        } catch (err) {
+          res.status(500).send(err.message);
+        }
+      
+      })
+    
--- src/routes/customersRoute.js
+++ src/routes/customersRoute.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { customersSchema } from "../schemas/customersSchema.js"
 import { getCustomers } from "../controllers/customersController.js"
+import { getCustomersById } from "../controllers/customersController.js"
 
 
 const customersRoute = Router()
 
 customersRoute.get("/customers", getCustomers)
+
+customersRoute.get("/customers/:id", getCustomersById)
 
 export default customersRoute
