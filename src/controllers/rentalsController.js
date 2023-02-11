5ee0015e629eb6a44c36acd37b988a51b81fafb8
Nanda-Souza <fesouoli@gmail.com>
2023-02-11 17:23:30 -0300

    feat: Method postRentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { db } from "../config/database.js"
 
 
@@ -14,9 +15,59 @@ export const getRentals = (async (req, res) => {
         
         
     
-        res.send(rentals.rows);
+        res.status(200).send(rentals.rows);
       } catch (err) {
         res.status(500).send(err);
 
       }
     });
+
+
+    export const createRentals = (async (req, res) => {
+        const { customerId,
+                gameId,
+                daysRented } = req.body
+        
+        const rentDate = dayjs().format("YYYY-MM-DD")
+        
+        
+        try {
+
+            const customerExist = await db.query(`SELECT name FROM customers WHERE id = $1`, [customerId]);
+
+            if (customerExist.rows.length === 0) {
+                return res.status(400).send("Customer not registered!");
+              }
+
+            const gameExist = await db.query(`SELECT "pricePerDay", "stockTotal" FROM games WHERE id = $1`, [gameId]);
+
+            if (gameExist.rows.length === 0) {
+                return res.status(400).send("Game not registered!");
+              }
+            
+            const originalPrice = daysRented * gameExist.rows[0].pricePerDay
+            
+            const rentalAvailable = await db.query(
+                `select g."stockTotal" from rentals r
+                JOIN games g ON r."gameId" = g.id
+                where g.id = $1`, [gameId]);
+            
+            if (rentalAvailable.rowCount > 0){
+                if(rentalAvailable.rowCount >= rentalAvailable.rows[0].stockTotal){
+                    return res.status(400).send("No games in stock available for rent!");
+                }
+            }
+
+            await db.query(
+                `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "originalPrice") 
+                 VALUES ($1, $2, $3, $4, $5)`, [customerId, gameId, rentDate, daysRented, originalPrice]);
+           
+            
+            res.sendStatus(201)            
+          } catch (err) {
+            res.status(500).send(err);
+    
+          }
+        });
+    
+
--- src/routes/rentalsRoute.js
+++ src/routes/rentalsRoute.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
-import { getRentals } from "../controllers/rentalsController.js";
+import { rentalsSchema } from "../schemas/rentalsSchema.js"
+import { getRentals,
+         createRentals } from "../controllers/rentalsController.js";
 
 
 
 const rentalsRoute = Router()
 
 rentalsRoute.get("/rentals", getRentals)
+
+rentalsRoute.post("/rentals", validateSchema(rentalsSchema), createRentals)
 
 export default rentalsRoute
